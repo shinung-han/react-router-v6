6ab379871eb105f99ba18af17fdee8e0a27bcae2
shinung-han <hanshinung@gmail.com>
2022-11-29 16:40:16 +0900

    add files

---
+++ src/pages/Article.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function Article() {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h2>게시글 {id}</h2>
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>홈</h1>
+      <p>가장 먼저 보여지는 페이지입니다.</p>
+      <ul>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles/velopert">velopert의 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profiles/gildong">gildong 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profiles/voild">존재하지 않는 프로필</Link>
+        </li>
+        <li>
+          <Link to="/articles">게시글 목록</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
